2cc58303ac52ea235c6ebca4bd00ea5f72ec0ea8
ivan <info@yoshi.tech>
2016-04-05 14:33:58 +0200

    update docs

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","SmsOrange\\Bookable"],["c","SmsOrange\\Cruise"],["c","SmsOrange\\Dispatcher"],["c","SmsOrange\\Main"]];
+ApiGen.elements = [["c","SmsOrange\\Bookable"],["c","SmsOrange\\Cruise"],["c","SmsOrange\\Cruise\\CostaCruisesWebservice"],["c","SmsOrange\\Cruise\\ManualCruisesWebservice"],["c","SmsOrange\\Dispatcher"],["c","SmsOrange\\Main"]];
